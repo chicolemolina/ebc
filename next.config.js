efabf39404822d036ed0ef73685639c9a180a2d8
unknown <chicolemolina>
2024-03-05 15:51:07 +0100

    pwa7

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
@@ -18,25 +17,4 @@ const nextConfig = withPWA({
     defaultLocale: "fr",
   },
 });
-=======
-/** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
-  disable: process.env.NODE_ENV === "development",
-});
-
-const nextConfig = withPWA({
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["fr"],
-    defaultLocale: "fr",
-  },
-});
->>>>>>> 121ff7ae7cff3aa94dbc988932bb29e7636e0b73
 module.exports = nextConfig;
