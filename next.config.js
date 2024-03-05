121ff7ae7cff3aa94dbc988932bb29e7636e0b73
Jose Molina <chicolemolina@gmail.com>
2024-03-05 15:26:15 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
-const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
-	reactStrictMode: true,
-	pwa: {
-		dest: "public",
-		register: true,
-		skipWaiting: true,
-		runtimeCaching,
-		buildExcludes: [/middleware-manifest.json$/]
-	}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
 });
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["fr"],
+    defaultLocale: "fr",
+  },
+});
+module.exports = nextConfig;
